b9eadd7873055672a0247a8c1a1bd97976641b22
Zhao <z.zheng@student.unsw.edu.au>
2016-05-08 20:07:03 +1000

    Update testGame.c

--- testGame.c
+++ testGame.c
@@ -1,24 +1,33 @@
-/* Functions that are prototyped/declared in the header file so we need to write the tests for
-   Game newGame (int discipline[], int dice[]);
-  void disposeGame (Game g);
-  void makeAction (Game g, action a); //where action will be an already defined struct
-  void throwDice (Game g, int diceScore);
-  int getDiscipline (Game g, int regionID);
-  int getDiceValue (Game g, int regionID);  
-  int getMostARCs (Game g);
-  int getMostPublications (Game g);
-  int getTurnNumber (Game g);
-  int getWhoseTurn (Game g);
-  int getCampus(Game g, path pathToVertex);
-  int getARC(Game g, path pathToEdge);
-  int isLegalAction (Game g, action a);
-  int getKPIpoints (Game g, int player);
-  int getARCs (Game g, int player);
-  int getGO8s (Game g, int player);
-  int getCampuses (Game g, int player);
-  int getIPs (Game g, int player);
-  int getPublications (Game g, int player);
-  int getStudents (Game g, int player, int discipline);
-  int getExchangeRate (Game g, int player, 
-                       int disciplineFrom, int disciplineTo);
-*/                     
+//Comment
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include <string.h>
+#include "Game.h"
+
+/*To test
+Game newGame (int discipline[], int dice[]);
+void disposeGame (Game g);
+void makeAction (Game g, action a); //where action will be an already defined struct
+void throwDice (Game g, int diceScore);
+int getDiscipline (Game g, int regionID); //This should return something in the interval [0,5]
+int getDiceValue (Game g, int regionID);  //This should return something in the interval [1,12]
+int getMostARCs (Game g);                 //This should find the greatest amount of arcs in Game g 
+int getMostPublications (Game g);         //Same as above
+int getTurnNumber (Game g);               //A number that is constantly kept in Game g
+int getWhoseTurn (Game g);                //Divide it through? Turn -1 is before Start, Turn 0 is Player 1's turn
+int getCampus(Game g, path pathToVertex); //This should return 0 for empty vertex, or 1/2/3 for the respective player
+int getARC(Game g, path pathToEdge);      //Same as above
+int isLegalAction (Game g, action a);     //Return 0 if legal
+int getKPIpoints (Game g, int player);    //Search Game g, for player's KPIpoints
+int getARCs (Game g, int player);         //Similar to above - but this means that Game struct stores where the campuses are
+int getGO8s (Game g, int player);         //Similar to above - since player is an int
+int getCampuses (Game g, int player);     //Similar to above
+int getIPs (Game g, int player);
+int getPublications (Game g, int player);
+int getStudents (Game g, int player, int discipline);
+int getExchangeRate (Game g, int player, 
+                     int disciplineFrom, int disciplineTo);
+*/
