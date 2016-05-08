214a23235e6d6d1cafcea45b5daba695b029fb99
Zhao <z.zheng@student.unsw.edu.au>
2016-05-08 19:24:16 +1000

    Create testGame.c

---
+++ testGame.c
@@ -0,0 +1,24 @@
+/* Functions that are prototyped/declared in the header file so we need to write the tests for
+   Game newGame (int discipline[], int dice[]);
+  void disposeGame (Game g);
+  void makeAction (Game g, action a); //where action will be an already defined struct
+  void throwDice (Game g, int diceScore);
+  int getDiscipline (Game g, int regionID);
+  int getDiceValue (Game g, int regionID);  
+  int getMostARCs (Game g);
+  int getMostPublications (Game g);
+  int getTurnNumber (Game g);
+  int getWhoseTurn (Game g);
+  int getCampus(Game g, path pathToVertex);
+  int getARC(Game g, path pathToEdge);
+  int isLegalAction (Game g, action a);
+  int getKPIpoints (Game g, int player);
+  int getARCs (Game g, int player);
+  int getGO8s (Game g, int player);
+  int getCampuses (Game g, int player);
+  int getIPs (Game g, int player);
+  int getPublications (Game g, int player);
+  int getStudents (Game g, int player, int discipline);
+  int getExchangeRate (Game g, int player, 
+                       int disciplineFrom, int disciplineTo);
+*/                     
